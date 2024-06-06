8f8d466aad0612cdcae2cef1786d1ec9d8818bd4
Paula I <illanesrpaula@gmail.com>
2024-06-06 19:33:49 -0300

    feat: sets timer from 0 to infinite. can pause and start from 0

--- src/app/components/principal.tsx
+++ src/app/components/principal.tsx
@@ -1,23 +1,53 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import css from "./principal.module.css"
+// Imports
+
+import { useState, useEffect } from "react";
+import css from "./principal.module.css";
+
+// Component -> Name: Principal
 
 const Principal = () => {
-    const [timer, setTimer] = useState(0)
+  const [timer, setTimer] = useState(0);
+  const [saveTime, lastSaveTime] = useState(0);
+  const [pause, setPause] = useState(false);
 
-   useEffect(() => {
-    const intervalId = setInterval(() => {
-        setTimer((time: number) => time + 1);
+  // TODO: When you pause it tracks the previous number, ex: you paused on 6, it'll save 5
+  // so when you want to continue, 5 is the number is going to appear
+  useEffect(() => {
+    let intervalId: any = null;
+    if (!pause) {
+       intervalId = setInterval(() => {
+        setTimer((time: any) => time + 1);
       }, 1000);
       return () => {
         clearInterval(intervalId);
       };
-   }, [timer])
+    } else {
+        clearInterval(intervalId);
+    }
+  }, [timer, pause]);
 
-    return (
-        <div className={css.principal}>{timer}</div>
-    )
-}
+  //Todo: Change functions names
+  const pauseAndSaveLastTime = () => {
+    setPause(true);
+    lastSaveTime(timer);
+  };
 
-export default Principal
+  const continueTimer = () => {
+    setPause(false);
+    setTimer(saveTime);
+  };
+
+  //Todo: Change style! Asap 
+  return (
+    <div>
+      <div className={css.principal}>{timer}</div>
+      <button onClick={() => pauseAndSaveLastTime()}>Pause</button>
+      <button onClick={() => continueTimer()}>Continue</button>
+      <button onClick={() => setTimer(0)}>Start Over</button>
+    </div>
+  );
+};
+
+export default Principal;
