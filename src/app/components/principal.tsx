6b3ddb6b2cf140014849063f24c83bd6b94529bc
Paula I <illanesrpaula@gmail.com>
2024-06-07 00:49:05 -0300

    feat: adding localstorage functionality

--- src/app/components/principal.tsx
+++ src/app/components/principal.tsx
@@ -3,20 +3,29 @@
 // Imports
 
 import { useState, useEffect } from "react";
+import { useLocalStorage } from "@/app/hooks/useLocalStorage";
 import css from "./principal.module.css";
 import ModalSettings from "@/app/components/Settings/ModalSettings";
 
 // Component -> Name: Principal
 
 const Principal = () => {
+  const [workTime, setWorkTime] = useLocalStorage("workTimer");
+  const [restTime, setValue] = useLocalStorage("restTimer");
   //Work
   const [workTimer, setWorkTimer] = useState(25); //25 seconds is the default time for countdown
   //Rest
-  const [restTime, setRestTime] = useState(5); //5 seconds is the default time for resting
+  const [restTimer, setRestTime] = useState(5); //5 seconds is the default time for resting
   //Other
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [pause, setPause] = useState(false)
+  const [pause, setPause] = useState(false);
 
+  useEffect(() => {
+    if (workTime !== undefined && restTime !== undefined) {
+      setWorkTimer(workTime);
+      setRestTime(restTime);
+    }
+  }, []);
 
   // Runs WORK time
   useEffect(() => {
@@ -30,21 +39,20 @@ const Principal = () => {
         clearInterval(intervalId);
       };
     }
-    if (workTimer === 0 && restTime === 0) {
-      console.log("Line 41")
-      setRestTime(5)
+    if (workTimer === 0 && restTimer === 0) {
+      console.log("Line 41");
+      setRestTime(restTime);
     }
     // CLear always
     return () => {
       clearInterval(intervalId);
     };
-  }, [workTimer, restTime, pause]);
+  }, [workTimer, restTimer, pause]);
 
   // Runs REST time
   useEffect(() => {
     let intervalId: ReturnType<typeof setTimeout>;
-    if (!pause && workTimer <= 0 && restTime > 0) {
-      console.log("Inside rest")
+    if (!pause && workTimer <= 0 && restTimer > 0) {
       intervalId = setInterval(() => {
         setRestTime((time: any) => time - 1);
       }, 1000);
@@ -52,24 +60,32 @@ const Principal = () => {
         clearInterval(intervalId);
       };
     }
-    if (restTime === 0 && workTimer === 0) {
-      setWorkTimer(25)
+    if (restTimer === 0 && workTimer === 0) {
+      setWorkTimer(workTime);
     }
     // CLear always
     return () => {
       clearInterval(intervalId);
     };
-   }, [restTime, workTimer, pause]);
-
+  }, [restTimer, workTimer, pause]);
 
   // Modal
   const onSaveModal = (work: number, rest: number) => {
     setIsModalOpen(false);
     setWorkTimer(work);
     setRestTime(rest);
+    setPause(false);
   };
 
+  const onOpenModal = () => {
+    setPause(true)
+    setIsModalOpen(true)
+  }
 
+  const onCloseModal = () => {
+    setPause(false)
+    setIsModalOpen(false)
+  }
 
   //Todo: Change style! Asap
   return (
@@ -77,19 +93,19 @@ const Principal = () => {
       {isModalOpen && (
         <ModalSettings
           saveModal={(work, rest) => onSaveModal(work, rest)}
-          closeModal={() => setIsModalOpen(false)}
+          closeModal={() => onCloseModal()}
         />
       )}
       <div>
-        {restTime > 0  && workTimer <= 0 ? (
-          <p className={css.principal}>Descanso: {restTime}</p>
+        {restTimer > 0 && workTimer <= 0 ? (
+          <p className={css.principal}>Descanso: {restTimer}</p>
         ) : (
           <p className={css.principal}>Trabajo: {workTimer}</p>
         )}
         <button onClick={() => setPause(true)}>Pause</button>
         <button onClick={() => setPause(false)}>Continue</button>
         <button onClick={() => setWorkTimer(0)}>Start Over</button>
-        <button onClick={() => setIsModalOpen(true)}>Open settings</button>
+        <button onClick={() => onOpenModal()}>Open settings</button>
       </div>
     </>
   );
